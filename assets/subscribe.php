a3cbed301f9c865ec2aedf2b9124141c5da2e39f
Susant Kadam <sushantkadam15@gmail.com>
2023-07-04 14:01:22 -0400

    Update subscribe.php

--- assets/subscribe.php
+++ assets/subscribe.php
@@ -57,30 +57,28 @@
                   <input id="email" name="email" type="email" placeholder="name@gmail.com" class="w-full bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-indigo-200 focus:bg-transparent border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required>
                 </div>
                 <button type="submit" name="submit" class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline none hover:bg-indigo-600 rounded text-lg">Remind Me</button>
-          </form>
-          <?php
-        }
-        ?>
-      </div>
-    </div>
-  </div>
-</section>
-
-<!-- Footer -->
-<footer class="text-gray-600 body-font mt-20">
-        <div class="container px-5 py-8 mx-auto flex items-center justify-center sm:flex-row flex-col">
-          <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <img src="./img/Logo.svg" alt="Logo" class="h-10">
-            </svg>
-          </a>
-          <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Ritual Mart —
-            <a href="mailto:ritualmart.store@gmail.com" class="text-gray-600 ml-1" rel="noopener noreferrer">@ritualmart</a>
-          </p>
-          <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-            <a href="https://www.linkedin.com/in/sushant-p-kadam/" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank"> 👨‍💻 <span class="text-[#008080]">Sushant Kadam</span></a>
-          </p>
+              </form>
+            <?php
+            }
+            ?>
+          </div>
         </div>
-    </footer>
-</body>
-</html>
+      </div>
+    </section>
 
+    <!-- Footer -->
+    <footer class="text-gray-600 body-font mt-20">
+      <div class="container px-5 py-8 mx-auto flex items-center justify-center sm:flex-row flex-col">
+        <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <img src="./img/Logo.svg" alt="Logo" class="h-10">
+        </a>
+        <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Ritual Mart —
+          <a href="mailto:ritualmart.store@gmail.com" class="text-gray-600 ml-1" rel="noopener noreferrer">@ritualmart</a>
+        </p>
+        <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+          <a href="https://www.linkedin.com/in/sushant-p-kadam/" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank"> 👨‍💻 <span class="text-[#008080]">Sushant Kadam</span></a>
+        </p>
+      </div>
+    </footer>
+  </body>
+</html>
